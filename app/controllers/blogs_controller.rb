a0c4ad5b2b828b2aeff6aa216727453ad4b0d784
Anri Liy <amedamachip713@gmail.com>
2020-02-17 18:28:10 -0800

    Add blogs model, controller, index and new views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,31 @@
 class BlogsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index 
+    @blogs = Blog.all
+  end
+
+  def show  
+  end
 
   def new 
+    @blog = Blog.new
   end 
+
   def create 
+    @blog = current_user.blogs.build(blog_params)
+    if @blog.save 
+      flash[:success] = 'Article was successfully created.'
+      redirect_to blogs_path 
+    else  
+      flash[:danger] = 'Article was not created.'
+      render :new
+    end 
+  end
+
+  private 
+
+  def blog_params 
+    params.require(:blog).permit(:title, :content)
   end
 end
